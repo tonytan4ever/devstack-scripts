81fa7d605dfe7e01a26ac1fb411a2ff3efe94e6a
tonytan4ever <tonytan198211@gmail.com>
2016-07-13 22:47:51 -0400

    Add two useful scripts

--- get_auth_token.sh
+++ get_auth_token.sh
@@ -4,4 +4,7 @@
 export OS_USERNAME=admin
 export OS_AUTH_URL=http://localhost:35357
 
+# cd into your devstack directory
+source openrc admin admin
+
 openstack token issue
---
+++ python_uninstall_scripts.sh
@@ -0,0 +1,6 @@
+
+# When you are doing install:
+# python setup.py install --record files.txt
+
+# cd into the directory where setup.py resides
+cat files.txt | xargs rm -rf
